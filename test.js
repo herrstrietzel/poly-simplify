c812b5e717debde8b9d8085dac8e832084bc7120
herrstrietzel <der@herrstrietzel.de>
2025-06-11 04:30:26 +0200

    npm test file and version

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-import polySimplify from './dist/poly-simplify.esm.js'; // or use require() if testing CJS
-
+import { polySimplify } from 'poly-simplify'; 
 const ptsSimplified = polySimplify(`M0 0 h10 h10 v10 v10`);
 console.log('Simplified Points:', ptsSimplified);
